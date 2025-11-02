2d46be7a78c87a15d32daa2e99bb4b3cb6d340ca
S1T <S1T>
2025-11-02 14:30:36 +0800

    tested

--- src/pages/user/Profile.tsx
+++ src/pages/user/Profile.tsx
@@ -4,7 +4,7 @@ const Profile: React.FC = () => {
   return (
     <div className="text-white font-host bg-gradient-to-br from-gray-900 via-gray-900 to-green-800 h-full w-full p-10">
       <p>Profile</p>
-      <p>Welcome to your profile page. Here you can view your account details and recent activity.</p>
+      <p>Welcome to your Profile page. Here you can view your account details and recent activity.</p>
     </div>
   );
 };
